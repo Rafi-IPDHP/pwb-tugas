7f290920e732762b0ff2b9cebcbde60d701a32ca
Rafi IPDHP <rafiislamipasha27@gmail.com>
2023-09-05 10:39:28 +0700

    s

--- resources/views/template/page/buku.blade.php
+++ resources/views/template/page/buku.blade.php
@@ -4,6 +4,35 @@
 
 @section('content')
     <section class="content-wrapper">
-        <h1>BLABLABLA</h1>
+        <h3 class="pt-3 d-flex align-items-center justify-content-center">Form Input Buku</h3>
+        <form action="" method="post" class="py-3 container">
+            <div class="mb-2">
+                <label for="kode_buku" class="form-label">Kode Buku</label>
+                <input type="number" class="form-control" id="kode_buku" placeholder="0123">
+            </div>
+            <div class="mb-2">
+                <label for="judul_buku" class="form-label">Judul Buku</label>
+                <input type="text" class="form-control" id="judul_buku" placeholder="Nyerah Aja">
+            </div>
+            <div class="mb-2">
+                <label for="penulis" class="form-label">Penulis Buku</label>
+                <input type="text" class="form-control" id="penulis" placeholder="Aldo">
+            </div>
+            <div class="mb-2">
+                <label for="penerbit" class="form-label">Penerbit Buku</label>
+                <input type="text" class="form-control" id="penerbit" placeholder="Aldo Ahmad">
+            </div>
+            <div class="mb-2">
+                <label for="tahun_terbit" class="form-label">Tahun Terbit</label>
+                <input type="number" class="form-control" id="tahun_terbit" placeholder="1999">
+            </div>
+            <div class="mb-4">
+                <label for="stok" class="form-label">Stok</label>
+                <input type="number" class="form-control" id="stok" placeholder="12">
+            </div>
+            <div class="d-flex align-items-end justify-content-end">
+                <button type="submit" class="btn btn-primary mb-3">Kirim</button>
+            </div>
+        </form>
     </section>
 @endsection
--- resources/views/template/page/petugas.blade.php
+++ resources/views/template/page/petugas.blade.php
@@ -4,6 +4,27 @@
 
 @section('content')
     <section class="content-wrapper">
-        <h1>INI PETUGAS</h1>
+        <h3 class="pt-3 d-flex align-items-center justify-content-center">Form Input Petugas</h3>
+        <form action="" method="post" class="py-3 container">
+            <div class="mb-2">
+                <label for="nama_petugas" class="form-label">Nama Petugas</label>
+                <input type="text" class="form-control" id="nama_petugas" placeholder="Aldo">
+            </div>
+            <div class="mb-2">
+                <label for="jabatan_petugas" class="form-label">Jabatan Petugas</label>
+                <input type="text" class="form-control" id="jabatan_petugas" placeholder="RPL">
+            </div>
+            <div class="mb-2">
+                <label for="no_telp_petugas" class="form-label">No Telephone</label>
+                <input type="text" class="form-control" id="no_telp_petugas" placeholder="08____">
+            </div>
+            <div class="mb-4">
+                <label for="alamat_petugas" class="form-label">Alamat Petugas</label>
+                <input type="text" class="form-control" id="alamat_petugas" placeholder="Dimana kek">
+            </div>
+            <div class="d-flex align-items-end justify-content-end">
+                <button type="submit" class="btn btn-primary mb-3">Kirim</button>
+            </div>
+        </form>
     </section>
 @endsection
