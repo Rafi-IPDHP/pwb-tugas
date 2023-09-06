bd99e1ed31392e0f795432fc7e8600411efeb5ff
Rafi IPDHP <rafiislamipasha27@gmail.com>
2023-09-06 16:49:46 +0700

    s

--- resources/views/template/separate/navbar.blade.php
+++ resources/views/template/separate/navbar.blade.php
@@ -5,7 +5,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Perpustakaan</a>
+        <a href="{{ route('blank_page') }}" class="nav-link">Home</a>
       </li>
     </ul>
 
